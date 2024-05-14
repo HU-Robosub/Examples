4758180f19e9ed28ce8c623250bcb4628bbd80fa
murunu <morenoschenk@gmail.com>
2024-05-14 11:48:49 +0200

    Add pid Code

---
+++ pid/src/mpu.cpp
@@ -0,0 +1,13 @@
+class Accelerometer {
+    public:
+        Accelerometer(){
+            // Initialize accelerometer
+        }
+        void setAngle(int angle){
+            // Set angle
+        }
+        int getAngle(){
+            // Get angle
+            return 0;
+        }
+};

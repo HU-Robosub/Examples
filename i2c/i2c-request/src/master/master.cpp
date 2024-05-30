5ac63c9d76c5d4820fe7b81dd714203969deb941
Lars Nijhuis <larsnijhui76@gmail.com>
2024-05-30 17:48:18 +0200

    na gilde troubleshoot

--- i2c/i2c-request/src/master/master.cpp
+++ i2c/i2c-request/src/master/master.cpp
@@ -1,38 +1,60 @@
 #include <Arduino.h>
 #include <Wire.h>
+int readData = 77;
+
 
 void sendRequest(int slave, int length, int reg) {
   Serial.println("Sending a register byte!");
+
   Wire.beginTransmission(slave);
   Wire.write(reg);
   Wire.endTransmission();
+
+  Serial.printf("Register byte sent: %d\n", reg);
+
   Serial.println("Initiated a request!");
-  Wire.requestFrom(slave, length);
-  byte answer[length];  // Declare an array to hold the received bytes
+  uint8_t a = Wire.requestFrom(slave, length);
+  Serial.println("Request sent!");
+  Serial.printf("Request status: %d\n", a);
 
-  int i = 0;
-  while (Wire.available()) {    // While there are bytes available to read
-    answer[i++] = Wire.read();  // Read a byte and store it in the array
+  while (Wire.available())
+  {
+    readData = Wire.read();
+    Serial.print("Data: ");
+    Serial.println(readData);
   }
+  // byte answer[length];  // Declare an array to hold the received bytes
 
-  Serial.print("The answer: ");
-  for (byte b : answer) {  // Loop over each byte in the array
-    Serial.print(b, DEC);  // Print the byte value in hexadecimal format
-    Serial.print(", ");   // Add a space between values for readability
-  }
-  Serial.println();
+  // int i = 0;
+  // while (Wire.available()) {    // While there are bytes available to read
+  //   answer[i++] = Wire.read();  // Read a byte and store it in the array
+  // }
+
+  // Serial.print("The answer: ");
+  // for (byte b : answer) {  // Loop over each byte in the array
+  //   Serial.print(b, DEC);  // Print the byte value in hexadecimal format
+  //   Serial.print(", ");   // Add a space between values for readability
+  // }
+  // Serial.println();
 }
 
 void setup() {
   Wire.begin();
   Serial.begin(9600);
-  while (!Serial) {
+  while (!Serial)
+  {
     delay(3000);
   }
+  delay(2000);
+
+
+  Serial.println("Request processed!");
 }
 
 void loop() {
-  sendRequest(0x05, 1, 0x02);
+  sendRequest(5, 1, 0);
+  sendRequest(5, 1, 1);
+  Serial.println("");
+  // sendRequest(5, 1, 2);
   delay(2000);
-  Serial.println("Request processed!");
 }
--- i2c/i2c-request/src/slave/slave.cpp
+++ i2c/i2c-request/src/slave/slave.cpp
@@ -1,51 +1,52 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-#define SLAVE_ADDRESS 0x05
+#define SLAVE_ADDRESS 5
 
-int reg = 0;
+volatile int reg;
 
-int accel_data[3] = {
-  10,
-  20,
-  12
-};
+// int accel_data[3] = {
+//   10,
+//   20,
+//   12
+// };
 void onReceiveEvent(int numBytes) {
   Serial.println("Recieved a byte!");
-  while (Wire.available()) {
+  while (Wire.available())
+  {
     reg = Wire.read();
-    Serial.print("Register byte: ");
-    Serial.println(reg);
   }
-  Serial.println("Request processed!");
+  Serial.print("Register byte: ");
+  Serial.println(reg);
+  Serial.println("Receive processed!");
 }
 
 void onRequestEvent() {
   Serial.println("Recieved a request!");
   if (reg == 0) {
-    Serial.println("Sending accel data!");
-    for (int i = 0; i < 3; i++) {
-      Wire.write(accel_data[i]);
-    }
+    Serial.println("Writing 0!");
+    Wire.write(0);
   }
   else if (reg == 1) {
-    Serial.println("Sending a 55 byte!");
-    Wire.write(55);
+    Serial.println("Writing 1!");
+    Wire.write(1);
   }
   else if (reg == 2) {
-    Serial.println("Sending a 66 byte!");
-    Wire.write(66);
+    Serial.println("Writing 2!");
+    Wire.write(2);
   }
   else {
-    Serial.println("Sending a 80 byte!");
-    Wire.write(80);
+    Serial.println("Writing 3!");
+    Wire.write(3);
   }
+  Serial.println("");
 }
 
 void setup() {
   Wire.begin(SLAVE_ADDRESS);
   Serial.begin(9600);
-  while (!Serial) {
+  while (!Serial)
+  {
     delay(3000);
   }
   Serial.println("Initialized Serial");
